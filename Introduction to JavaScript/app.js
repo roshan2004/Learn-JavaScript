b37b7edbd6e21acab3aa6b34e2757ab1181e6886
Roshan Shrestha <roshanpra@gmail.com>
2021-06-14 21:59:19 +0545

    Intro to JavaScript

---
+++ Introduction to JavaScript/app.js
@@ -0,0 +1,17 @@
+console.log(33);
+console.log(1); // prints 1 on the screen
+/* This is a multi-line
+com that
+I am trying to make in here
+*/
+console.log('My Name is Roshan Shrestha');
+console.log(3.5+33);
+console.log('One'+', '+'two'+', '+'three!');
+console.log('middle'+' '+'space');
+console.log('Hello'.length);
+console.log('Roshan'.toUpperCase());
+console.log('Hey'.startsWith('H'));
+console.log('   Removes space   '.trim());
+console.log(Math.floor(Math.random() * 50));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
